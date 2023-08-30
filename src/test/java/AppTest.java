84b7e04186e79b3687aefa9bb37ff388f9cb0ded
Mandarin <msdolphin@yandex.ru>
2023-08-30 20:03:03 +0300

    mm

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -13,7 +13,7 @@ public class AppTest {
 
     @Autowired
     private ApplicationContext applicationContext;
-//тест kfkfkfkfkf
+//тест ignore
     @Test
     public void mainTest() {
         HelloWorld tree =
